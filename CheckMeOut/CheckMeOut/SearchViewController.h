edc5a19d23275b7d97c501788e25f73deb2e0b3d
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-10-10 14:26:36 -0400

    created a SearchViewController class and set up some IBOUtlets to be connected to storyboard later along with UICollectionView datasource methods

---
+++ CheckMeOut/CheckMeOut/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  CheckMeOut
+//
+//  Created by Jackie Meggesto on 10/10/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ CheckMeOut/CheckMeOut/SearchViewController.m
@@ -0,0 +1,49 @@
+//
+//  SearchViewController.m
+//  CheckMeOut
+//
+//  Created by Jackie Meggesto on 10/10/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import "SearchViewController.h"
+#import <Parse/Parse.h>
+#import <AFNetworking/AFNetworking.h>
+
+
+@interface SearchViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate>
+
+@property (nonatomic) IBOutlet UITextField* nameTextField;
+@property (nonatomic) IBOutlet UITextField* emailTextField;
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+-(void)fetchFacebookData {
+    
+}
+-(void)fetchLinkedInData {
+    
+}
+-(void)fetchTwitterData{
+    
+}
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+}
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    
+    
+}
+
+
+
+
+@end
